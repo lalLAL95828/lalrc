ce56fb9126cb85fce18314eb621714d6fe48a1b3
lallal_win10 <1445668063@qq.com>
2020-06-13 23:02:49 +0800

    lal

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -5,12 +5,17 @@ storiesOf("welcome page",module)
 .add("welcome",()=>{
     return (
         <>
-          <h1>欢迎来到 vikingship 组件库</h1>
-          <p>vikingship 是为慕课网课程打造的一套教学组件库，从零到一让大家去学习</p>
-          <h3>安装试试</h3>
+          <h1>欢迎来到 lalrc 组件库</h1>
+          <p>这款组件是在项目经验中，觉得某个功能在以后开发中会存在需求而开发的。借鉴了其他组件，复杂性高于其他组件。</p>
+          <p>组件不多，但做一个，精一个。组件会随着时间逐渐更新和增加。因为这是款React组件，所以暂时只能用于React项目。</p>
+          <h3>安装</h3>
           <code>
             npm install vikingship --save
           </code>
+          <h3>文档链接</h3>
+          <a href="https://lalLaL95828.github.io/lalrc" target="_self">lalrc文档</a>
+          <h3>git地址</h3>
+          <a href="https://github.com/lalLAL95828/lalrc" target="_blank" rel="noopener noreferrer">项目库</a>
         </>
       )
 },{
